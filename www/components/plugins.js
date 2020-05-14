545f0483f12e0c362a7e81d4b76c64423ca8d9f0
Fernanch-03 <fefemachado03@gmail.com>
2020-05-15 07:11:01 +0900

    Adição de plugins, porem com problemas na conexão entre arquivos

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,2 +1,29 @@
 // This is a JavaScript file
 
+ $(document).on("click","#alerta",function() {
+   
+   navigator.notification.alert("Minha mensagem",null,"aviso","aceito");
+   
+   });
+
+$(document).on("click","#confirm",function(){
+  function confirma(buttonIndex){
+    if(buttonIndex == 1){
+      navigator.notification.alert("Ola mundo");
+    }
+    if(buttonIndex == 2){
+      navigator.notification.alert("Hello word");
+    }
+  }
+   navigator.notification.confirm("escolha A ou B",confirma,"Escolha: ",['A','B']);
+});
+
+$(document).on("click","#beep",function(){
+  
+   navigator.notification.beep(2);
+});
+
+$(document).on("click","#vibrar",function(){
+  
+   navigator.vibrate(2);
+}); 
