a11ebc742247497b4b141342ab6ac93e72164f48
Fernanch-03 <fefemachado03@gmail.com>
2020-05-16 10:00:32 +0900

    termino dos plugins de localização, mas ainda não conecta com o celular

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -27,3 +27,29 @@ $(document).on("click","#vibrar",function(){
   
    navigator.vibrate(2);
 }); 
+
+function mostramapa(lat, long){
+  L.mapquest.key = '	sCQLgctps7lqkiXsc2HqzVGPADxudXOG';
+ 
+        var map = L.mapquest.map('map', {
+          center: [lat, long],
+          layers: L.mapquest.tileLayer('map'),
+          zoom: 20
+        });
+
+        map.addControl(L.mapquest.control());
+}
+
+$(document).on("click","#local",function(){
+    var onSuccess = function(position) {
+      mostramapa(position.coords.latitude  , position.coords.longitude)
+        
+    };
+
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}); 
